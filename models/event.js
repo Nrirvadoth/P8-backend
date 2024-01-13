f0185f3c8af7b1f6a583117e8ef9ad31d82f034a
Mat <mathurin.malandain+github@gmail.com>
2024-01-13 18:38:03 +0100

    correction key name eventSchema

--- models/event.js
+++ models/event.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const eventSchema = mongoose.Schema({
   date: { type: String, required: true },
   event: { type: String, required: true },
-  desc: { type: String, required: true },
+  description: { type: String, required: true },
 })
 
 module.exports = mongoose.model('Event', eventSchema)
