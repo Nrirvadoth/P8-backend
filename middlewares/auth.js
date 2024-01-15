ee0ead1013cf3eb8bf843967a507a98c6eaba2a4
Mat <mathurin.malandain+github@gmail.com>
2024-01-15 19:05:28 +0100

    correction affichage erreur auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
+  console.log(req.body)
   try {
     const token = req.headers.authorization.split(' ')[1]
     const tokenDecoded = jwt.verify(token, 'OCP8-token_1548re58e')
     const userId = tokenDecoded.userId
     req.auth = { userId: userId }
     next()
-  } catch {
+  } catch (error) {
     res.status(401).json({ error })
   }
 }
